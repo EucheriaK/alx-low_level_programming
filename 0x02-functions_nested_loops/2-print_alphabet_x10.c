da54d3638d1a5e41678acf3bfabfca8de24314a1
EucheriaK <ekureva@gmail.com>
2022-10-18 08:26:06 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,16 @@
 */
 void print_alphabet_x10(void)
 {
-	int n;
-	
-	for (n = 'a'; n <= 'z'; n++)
+	int n, co;
+
+	co = 0;
+	while (co < 10)
 	{
-		_putchar(n);
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		co++
+		_putchar('\n');
 	}
-	_putchar('\n');
 }

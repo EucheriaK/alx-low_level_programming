32c50e6f2bbefe26123e05c0975405528d2470d1
EucheriaK <ekureva@gmail.com>
2022-10-27 01:36:32 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+* main - Entry point
+* Description: Add one line to this code, so that the program prints a[2] = 98
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
+
 int n;
 int a[5];
 int *p;
@@ -17,9 +23,36 @@ p = &n;
 * - you are not allowed to code anything else than this line of code
 */
 
-p[5] = 98;
+*(p + 5) = 98;
+
 /* ...so that this prints 98\n */
+
 printf("a[2] = %d\n", a[2]);
 
 return (0);
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

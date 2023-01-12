bfbf7454dd7d1dde099785453b8e6f51bf8da50f
EucheriaK <ekureva@gmail.com>
2023-01-12 15:17:47 +0200

    1-dlistint_len.c

--- 1-dlistint_len.c
+++ 1-dlistint_len.c
@@ -1,22 +1,19 @@
 #include "lists.h"
-
 /**
- * dlistint_len - function that returns the number of elements in a,
- * linked dlistint_t list.
- * @h: poiner to the head of the list
- *
- * Return: number of elements
+ * dlistint_len - returns the number of elements in a linked dlistint_t list.
+ * @h: pointer to the head of the list
+ * Return: number of nodes
  */
 
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t count = 0;
-
-	/* while we have a pointer to the list */
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
+int counter = 0;
+if (h == NULL)
+return (counter);
+while (h != NULL)
+{
+counter++;
+h = h->next;
+}
+return (counter);
 }

691d57a069e378af09d1543ae5acf9e204398750
EucheriaK <ekureva@gmail.com>
2023-01-17 11:26:22 +0200

    dynamic

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
-gcc -g -fPIC -Wall -Wexra -Werror -pedantic -c fPIC *.c
+-Wall -Wextra -Werror -pedantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
